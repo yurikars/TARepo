053e3c4d3c960a03caa2c0144df469c7a12b7925
yurikars <buddy.yurik@gmail.com>
2017-12-26 23:58:48 +0300

    points distance method ready to review

--- sandbox/src/main/java/autoLineLenght/Point.java
+++ sandbox/src/main/java/autoLineLenght/Point.java
@@ -10,7 +10,7 @@ public class Point {
         this.y = y;
     }
 
-    public static double distance (Point p1, Point p2){
+    public static double distanceBetweenTwoPoints (Point p1, Point p2){
         double dx = Math.pow((p2.x - p1.x),2);
         double dy = Math.pow((p2.y - p1.y),2);
         return Math.sqrt(dx + dy);
@@ -20,7 +20,7 @@ public class Point {
         Point p1 = new Point(4,2);
         Point p2 = new Point(7,5);
 
-        System.out.println("Расстояние между точками p1 и p2 без округления равно: " + distance(p1,p2));
-        System.out.println("Расстояние между точками p1 и p2 с округлением равно: " + Math.round(distance(p1,p2)));
+        System.out.println("Расстояние между точками p1 и p2 без округления равно: " + distanceBetweenTwoPoints(p1,p2));
+        System.out.println("Расстояние между точками p1 и p2 с округлением равно: " + Math.round(distanceBetweenTwoPoints(p1,p2)));
     }
 }
