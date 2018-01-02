6f07a3e0cb04e01846d5816d1d6251c85ab1850c
yurikars <buddy.yurik@gmail.com>
2018-01-02 11:42:25 +0300

    updated test

--- sandbox/src/test/java/lineLenghtTest/PointTest.java
+++ sandbox/src/test/java/lineLenghtTest/PointTest.java
@@ -14,16 +14,18 @@ public class PointTest {
         Point p1 = new Point(8,3);
         Point p2 = new Point(5,1);
 
-        double notRoundedlineLenght = Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2));
-        double roundedLineLenght = Math.round(Math.sqrt(Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2)));
+        double notRoundedlineLenght = p1.distanceBetweenTwoPoints(p2);
+        double roundedLineLenght = Math.round(p1.distanceBetweenTwoPoints(p2));
 
-        System.out.println("Длина отрезка до без округления равна: " + notRoundedlineLenght);
+        System.out.println("Длина отрезка без округления равна: " + notRoundedlineLenght);
         System.out.println("Длина отрезка с округлением равна: " + roundedLineLenght);
 
         // проверка того, что округлённое значение длины отрезка равно 4
          Assert.assertEquals(roundedLineLenght,4.0);
 
+
         // проверка того, что не округлённое значение длины отрезка не равно 4
         Assert.assertNotEquals(notRoundedlineLenght,4.0);
+
     }
 }
