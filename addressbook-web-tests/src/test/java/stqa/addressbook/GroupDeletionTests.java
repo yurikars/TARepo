4e5ff15461c6317ddb2841dbecf98f13530ac24e
yurikars <buddy.yurik@gmail.com>
2018-01-04 23:34:08 +0300

    pull up commit

---
+++ addressbook-web-tests/src/test/java/stqa/addressbook/GroupDeletionTests.java
@@ -0,0 +1,18 @@
+package stqa.addressbook;
+
+import org.testng.annotations.Test;
+
+public class GroupDeletionTests extends TestBase {
+
+
+    @Test
+    public void testGroupDeletion() {
+
+        gotoGroupPage();
+        selectGroup();
+        deleteSelectedGroups();
+        returnToGroupPage();
+    }
+
+
+}
