fd2a6aec89e4364e3a715ffb501e6bdf63050b81
yurikars <buddy.yurik@gmail.com>
2018-01-05 10:34:36 +0300

    pull up contact's class + imports optimisation

--- addressbook-web-tests/src/test/java/stqa/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/ContactCreationTest.java
@@ -1,14 +1,6 @@
 package stqa.addressbook;
 
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
 
 public class ContactCreationTest extends TestBase {
 
