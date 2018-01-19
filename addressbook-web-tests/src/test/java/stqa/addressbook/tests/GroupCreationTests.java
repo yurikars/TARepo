73a47fd2bf8b15f9c8da12224a214ede66bcb718
yurikars <buddy.yurik@gmail.com>
2018-01-19 14:32:45 +0300

    selecting by index

--- addressbook-web-tests/src/test/java/stqa/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/tests/GroupCreationTests.java
@@ -10,7 +10,7 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
         int before = app.getGroupHelper().getGroupCount();
-        app.getGroupHelper().createGroup(new GroupData("Test1", null, null));
+        app.getGroupHelper().createGroup(new GroupData("test1", null, null));
         int after = app.getGroupHelper().getGroupCount();
         Assert.assertEquals(after,before + 1);
     }
