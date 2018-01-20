071791c7c7ea5f049864f87477ceadd567bf39cc
yurikars <buddy.yurik@gmail.com>
2018-01-20 12:23:17 +0300

    groupCreation failed test

--- addressbook-web-tests/src/test/java/stqa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/model/GroupData.java
@@ -3,22 +3,21 @@ package stqa.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    private       int id;
     private final String name;
     private final String header;
     private final String footer;
 
 
-
-    public GroupData(String id, String name, String header, String footer) {
-        this.id = id;
+    public GroupData(String name, String header, String footer) {
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
+    public GroupData(int id, String name, String header, String footer) {
 
-    public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = id;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -36,23 +35,12 @@ public class GroupData {
         return footer;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return Objects.equals(id, groupData.id) &&
-                Objects.equals(name, groupData.name);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(id, name);
+    public void setId(int id) {
+        this.id = id;
     }
 
     @Override
@@ -61,6 +49,21 @@ public class GroupData {
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return id == groupData.id &&
+                Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name);
     }
 
 }
