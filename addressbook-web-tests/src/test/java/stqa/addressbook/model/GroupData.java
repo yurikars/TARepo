052f318e1315d17221cabac65a5c6ae6706f9ee8
yurikars <buddy.yurik@gmail.com>
2018-01-20 15:25:39 +0300

    lambda function adding to the groupTests

--- addressbook-web-tests/src/test/java/stqa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/model/GroupData.java
@@ -9,8 +9,10 @@ public class GroupData {
     private final String footer;
 
 
+
     public GroupData(String name, String header, String footer) {
-        this.id = 0;
+        this.id = Integer.MAX_VALUE;
+
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -56,14 +58,14 @@ public class GroupData {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return id == groupData.id &&
-                Objects.equals(name, groupData.name);
+        return Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(id, name);
+        return Objects.hash(name);
     }
+
 
 }
