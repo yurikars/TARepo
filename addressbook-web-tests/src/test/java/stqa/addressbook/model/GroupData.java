330829816911ff7476b984a23e6003ff161a9db6
yurikars <buddy.yurik@gmail.com>
2018-01-20 11:57:39 +0300

    groupModification test updating

--- addressbook-web-tests/src/test/java/stqa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/model/GroupData.java
@@ -3,11 +3,22 @@ package stqa.addressbook.model;
 import java.util.Objects;
 
 public class GroupData {
+    private final String id;
     private final String name;
     private final String header;
     private final String footer;
 
+
+
+    public GroupData(String id, String name, String header, String footer) {
+        this.id = id;
+        this.name = name;
+        this.header = header;
+        this.footer = footer;
+    }
+
     public GroupData(String name, String header, String footer) {
+        this.id = null;
         this.name = name;
         this.header = header;
         this.footer = footer;
@@ -25,24 +36,31 @@ public class GroupData {
         return footer;
     }
 
+    public String getId() {
+        return id;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
+        return Objects.equals(id, groupData.id) &&
+                Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
 
-        return Objects.hash(name);
+        return Objects.hash(id, name);
     }
 
     @Override
     public String toString() {
         return "GroupData{" +
-                "name='" + name + '\'' +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
                 '}';
     }
+
 }
