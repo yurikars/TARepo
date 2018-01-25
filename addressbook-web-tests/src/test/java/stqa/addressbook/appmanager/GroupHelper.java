b9fa3ea15c0e0b6d1e5a7cfa6c3b9835d1dae8d5
yurikars <buddy.yurik@gmail.com>
2018-01-25 17:44:01 +0300

    Set instead of List for Group Tests

--- addressbook-web-tests/src/test/java/stqa/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/appmanager/GroupHelper.java
@@ -3,11 +3,11 @@ package stqa.addressbook.appmanager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import stqa.addressbook.model.GroupData;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class GroupHelper extends HelperBase {
 
@@ -38,8 +38,9 @@ public class GroupHelper extends HelperBase {
         click(By.name("delete"));
     }
 
-    public void selectGroup(int index) {
-        wd.findElements(By.name("selected[]")).get(index).click();
+    public void selectGroupById(int id) {
+
+        wd.findElement(By.cssSelector("input[value='" + id + "']")).click();
     }
 
     public void initGroupModification() {
@@ -56,16 +57,11 @@ public class GroupHelper extends HelperBase {
         submitGroupCreation();
         returnToGroupPage();
     }
-    public void modify(int index, GroupData group) {
-        selectGroup(index);
+    public void modify(GroupData group) {
+        selectGroupById(group.getId());
         initGroupModification();
         fillGroupForm(group);
         submitGroupModification();
-        returnToGroupPage();
-    }
-    public void delete(int index) {
-        selectGroup(index);
-        deleteSelectedGroups();
         returnToGroupPage();
     }
 
@@ -77,8 +73,9 @@ public class GroupHelper extends HelperBase {
         return wd.findElements(By.name("selected[]")).size();
     }
 
-    public List<GroupData> list() {
-        List<GroupData> groups = new ArrayList<GroupData>();
+
+    public Set<GroupData> all() {
+        Set<GroupData> groups = new HashSet<GroupData>();
         List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
         for (WebElement element : elements){
             String name = element.getText();
@@ -87,5 +84,11 @@ public class GroupHelper extends HelperBase {
             groups.add(new GroupData().withId(id).withName(name));
         }
         return groups;
+    }
+
+    public void delete(GroupData group) {
+        selectGroupById(group.getId());
+        deleteSelectedGroups();
+        returnToGroupPage();
     }
 }
--- addressbook-web-tests/src/test/java/stqa/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/stqa/addressbook/model/GroupData.java
@@ -40,9 +40,25 @@ public class GroupData {
         return this;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GroupData groupData = (GroupData) o;
+        return id == groupData.id &&
+                Objects.equals(name, groupData.name);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, name);
+    }
+
     public GroupData withFooter(String footer) {
         this.footer = footer;
         return this;
+
     }
 
     @Override
@@ -51,20 +67,6 @@ public class GroupData {
                 "id='" + id + '\'' +
                 ", name='" + name + '\'' +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        GroupData groupData = (GroupData) o;
-        return Objects.equals(name, groupData.name);
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(name);
     }
 
 
